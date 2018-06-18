b8c6e6990e43e727a2d7366612429bf6af4802e8
Shardul <iamsharduld@gmail.com>
2018-06-18 14:17:31 +0530

    initial

---
+++ functions/src/index.ts
@@ -0,0 +1,15 @@
+import * as functions from 'firebase-functions';
+import { BADSTR } from 'dns';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+ export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
+  
+
+
+
+ 
+
