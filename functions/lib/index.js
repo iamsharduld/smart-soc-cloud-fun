aef5172c94b6697d8872a78ee0697843d16f7d81
Shardul <iamsharduld@gmail.com>
2018-06-19 16:28:26 +0530

    "more APIs"

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -5,7 +5,6 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
 exports.getMembers = functions.https.onRequest((request, response) => {
     const db = admin.database();
     const MembersRef = db.ref("/Members");
@@ -13,6 +12,41 @@ exports.getMembers = functions.https.onRequest((request, response) => {
         .then(function (snap) {
         console.log(snap.val());
         response.status(200).json({ members: snap.val() });
+    })
+        .catch(function (error) {
+        console.log(error.code);
+    });
+});
+exports.getSociety = functions.https.onRequest((request, response) => {
+    const db = admin.database();
+    const SocietyRef = db.ref("/Society");
+    SocietyRef.once('value')
+        .then(function (snap) {
+        console.log(snap.val());
+        response.status(200).json({ Society: snap.val() });
+    })
+        .catch(function (error) {
+        console.log(error.code);
+    });
+});
+exports.getNotices = functions.https.onRequest((request, response) => {
+    const db = admin.database();
+    const NoticesRef = db.ref("/Society/services/notices");
+    NoticesRef.once('value')
+        .then(function (snap) {
+        console.log(snap.val());
+        response.status(200).json({ Notices: snap.val() });
+    })
+        .catch(function (error) {
+        console.log(error.code);
+    });
+});
+exports.getFacilities = functions.https.onRequest((request, response) => {
+    const db = admin.database();
+    const FacilitiesRef = db.ref("/Society/services/facilities");
+    FacilitiesRef.once('value')
+        .then(function (snap) {
+        response.status(200).json({ All_Facilities: snap.val() });
     })
         .catch(function (error) {
         console.log(error.code);
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+
 admin.initializeApp()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
+// All member APIs
 export const getMembers = functions.https.onRequest((request, response) => {
     const db = admin.database();
     const MembersRef = db.ref("/Members");
@@ -19,8 +21,63 @@ export const getMembers = functions.https.onRequest((request, response) => {
         console.log(error.code);
     })
 
+ });
+
+
+// All society APIs 
+ export const getSociety = functions.https.onRequest((request, response) => {
+    const db = admin.database();
+    const SocietyRef = db.ref("/Society");
+
+    SocietyRef.once('value')
+    .then(function(snap){
+        console.log(snap.val())
+        response.status(200).json({Society: snap.val()});
+    })
+    .catch(function(error){
+        console.log(error.code);
+    })
+
 
  });
+
+
+
+//All society APIs
+export const getNotices = functions.https.onRequest((request, response) => {
+    const db = admin.database();
+    const NoticesRef = db.ref("/Society/services/notices");
+
+    NoticesRef.once('value')
+    .then(function(snap){
+        console.log(snap.val())
+        response.status(200).json({Notices: snap.val()});
+    })
+    .catch(function(error){
+        console.log(error.code);
+    })
+
+
+ });
+
+
+//All society APIs
+export const getFacilities = functions.https.onRequest((request,response) => {
+    const db = admin.database();
+    const FacilitiesRef = db.ref("/Society/services/facilities");
+
+    FacilitiesRef.once('value')
+    .then(function(snap){
+        response.status(200).json({All_Facilities: snap.val()});
+    })
+    .catch(function(error){
+        console.log(error.code);
+    })
+
+});
+
+
+
 
 
 
